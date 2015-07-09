7cf4f564a480525f0003891d1f48824b35c3a12c
ThomKiesewetterPrive <thom@kiesewetter.nl>
2015-07-09 10:57:28 +0200

    Add Scripttag and THTag

---
+++ src/TagHelpers/ScriptTagHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Mvc.TagHelpers;
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace TagHelpers
+{
+    [TargetElement("script", Attributes = "asp-fallback-src")]
+    [TargetElement("script", Attributes = "asp-copy-src-to-fallback")]
+    public class ScriptTagHelper : TagHelper
+    {
+        private readonly string preTest = @"<SCRIPT>({0}==null||alert('{1}'));</SCRIPT>";
+        private readonly string postTest = @"<SCRIPT>({0}!=null||alert('{1}'));</SCRIPT>";
+        [HtmlAttributeName("asp-copy-src-to-fallback")]
+        public string CopySrcToFallback { get; set; }
+
+        [HtmlAttributeName("asp-warn-if-test-is-invalid")]
+        public string WarnIfTestIsInvalid { get; set; }
+
+        [HtmlAttributeName("asp-fallback-src")]
+        public string FallbackSrc { get; set; }
+
+        [HtmlAttributeName("asp-fallback-test")]
+        public string FallbackTest { get; set; }
+
+        [HtmlAttributeName("src")]
+        public string RemotePath { get; set; }
+
+        protected internal IHostingEnvironment HostingEnvironment { get; set; }
+        protected internal IHttpContextAccessor Context { get; set; }
+        public ScriptTagHelper(IHostingEnvironment hostingEnvironment, IHttpContextAccessor context)
+        {
+            HostingEnvironment = hostingEnvironment;
+            Context = context;
+        }
+        //
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (WarnIfTestIsInvalid?.Contains(HostingEnvironment.EnvironmentName, StringComparison.OrdinalIgnoreCase) ==
+                true)
+            {
+                var x = await context.GetChildContentAsync();
+                output.PreElement.AppendFormat(preTest, FallbackTest, $"Script `{RemotePath}` already loaded. Did you create the correct test");
+                output.PostElement.AppendFormat(postTest, FallbackTest, $"Script `{RemotePath}` still not loaded. Did you create the correct test");
+            }
+            if (CopySrcToFallback?.Contains(HostingEnvironment.EnvironmentName, StringComparison.OrdinalIgnoreCase) ==
+                true)
+            {
+                try
+                {
+                    RemotePath = RemotePath.StartsWith("/")
+                        ? Context.HttpContext.Request.Scheme + "://" + Context.HttpContext.Request.Host + RemotePath
+                        : RemotePath;
+
+                    FallbackSrc = FallbackSrc ?? "/fallback/js/" + new Uri(RemotePath).Segments.Last();
+                    var localPath = HostingEnvironment.MapPath(FallbackSrc.TrimStart('/'));
+                    var pathHelper = new PathHelper(RemotePath, localPath);
+                    if (!File.Exists(localPath))
+                    {
+                        using (var webClient = new HttpClient())
+                        {
+                            var file = await webClient.GetStringAsync(new Uri(RemotePath));
+                            Directory.CreateDirectory(pathHelper.LocalDirectory);
+                            File.WriteAllText(localPath, file);
+                        }
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    throw new FileNotFoundException($"The remote file:{RemotePath} cannot be found.", ex);
+                }
+                output.CopyHtmlAttribute("src", context);
+            }
+        }
+
+    }
+}
