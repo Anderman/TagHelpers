831424183910fd63a897bfaa42c6294828e5b249
Thom Kiesewetter <thom@kiesewetter.nl>
2015-08-21 18:43:23 +0200

    Remove outcomment code

--- src/TagHelpers/MaterialDesignInputTagHelper.cs
+++ src/TagHelpers/MaterialDesignInputTagHelper.cs
@@ -14,18 +14,6 @@ namespace Anderman.TagHelpers
 
         [HtmlAttributeName("type")]
         public string InputTypeName { get; set; }
-        /// <summary>
-        /// <div class="form-group form-md-line-input">
-        ///    <label class="col-md-2 control-label" asp-for="AccessFailedCount">Regular input</label>
-        ///    <div class="col-md-10">
-        ///        <input asp-for="AccessFailedCount" class="form-control"/>
-        ///        <div class="form-control-focus"></div>
-        ///    </div>
-        ///</div>
-        /// </summary>
-        /// <param name="context"></param>
-        /// <param name="output"></param>
-        /// <returns></returns>
 
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
@@ -34,7 +22,6 @@ namespace Anderman.TagHelpers
             var placeholder = For.Metadata.GetPlaceholder();
 
             await context.GetChildContentAsync();
-            //output.PreElement.Append("<div class='input-field col s6'>");
             if (!output.Content.GetContent().Contains("checkbox"))
             {
                 output.PreElement.Append("<div class='form-control-wrapper'>");
@@ -50,12 +37,6 @@ namespace Anderman.TagHelpers
             }
             else
             {
-                //< div class="checkbox">
-                //  <label>
-                //    <input type = "checkbox" checked="">
-                //    Auto -updates
-                //    </label>
-                //</div>
                 var togglebutton = false;
                 if (context.AllAttributes.ContainsName("class") && context.AllAttributes["class"].Value.ToString().Contains("togglebutton"))
                     togglebutton = true;
@@ -64,21 +45,10 @@ namespace Anderman.TagHelpers
                 output.PreElement.Append(@"<label>");
                 output.Content.SetContent(output.Content.GetContent().Replace("/><input", "/><span class='"+ (togglebutton ? "toggle" : "checkbox-material") + "'><span class=check></span></span><input"));
                 output.PostElement.Append(" " + displayname);
-                //
                 output.PostElement.Append($"<span class='text-danger field-validation-valid' data-valmsg-for='{For.Name}' data-valmsg-replace='true'></span>");
-                //
                 output.PostElement.Append(@"</label>");
                 output.PostElement.Append(@"</div>");
-
-                //output.Content.SetContent(output.Content.GetContent().Replace("form-control", "md-check"));
-                //output.PostElement.Append($"<label for='{For.Name}'>{displayname}</label>");
-                //output.PreElement.Append($"<label class='control-label' for='{For.Name}'></label>");
-                //output.PreElement.Append(@"<div class='md-checkbox'>");
-                //output.PostElement.Append($"<label for='{For.Name}'>{displayname}");
-                //output.PostElement.Append(@"<span></span><span class='check'></span><span class='box'></span></label></div>");
             }
-            //output.PostElement.Append(@"<div class='form-control-focus'></div>");
-            //output.PostElement.Append($"<span class='help-block'>{description}</span></div>");
             return;
         }
     }
